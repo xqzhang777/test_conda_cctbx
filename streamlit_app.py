d03c70984ae9fadd33296209b0f114bd1533377d
Wen Jiang <1855485+wjiang@users.noreply.github.com>
2022-07-26 09:50:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,29 @@
 import sys
-import shutil 
+import shutil
+from pathlib import Path
 import streamlit as st
 
 st.info(sys.path)
 st.info(sys.executable)
+st.info(dir(sys))
 st.info(shutil.which("python"))
 st.info(shutil.which("conda"))
 st.info(shutil.which("streamlit"))
 
-sys.path = [p for p in sys.path if p.find("3.7")==-1 and not p.startswith("/usr/local")]
-sys.path.append("/home/appuser/venv/lib/python3.9")
-st.info(sys.path)
+for p in sys.path:
+  p = Path(p)
+  if p.is_dir():
+    st.info(f"{p}: {Path(p).glob('*')}")
+  else:
+    st.info(f"{p})
 
-from pathlib import Path
+#sys.path = [p for p in sys.path if p.find("3.7")==-1 and not p.startswith("/usr/local")]
+#sys.path.append("/home/appuser/venv/lib/python3.9")
+#st.info(sys.path)
+
 target = Path("/home/appuser/venv/share/cctbx")
 if not target.exists():
   target.symlink_to("/home/appuser/venv/lib/python3.9/site-packages/cctbx")
-import cctbx
+
+            import cctbx
 import iotbx
