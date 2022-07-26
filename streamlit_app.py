80529d1e039cc7c531b70a65f1ff84bf7c606e2b
Wen Jiang <1855485+wjiang@users.noreply.github.com>
2022-07-25 23:00:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.info(sys.path)
 st.info(sys.executable)
 st.info(shutil.which("python"))
-st.info(shutil.which("cuda"))
+st.info(shutil.which("conda"))
 st.info(shutil.which("streamlit"))
 
 sys.path = [p for p in sys.path if p.find("3.7")==-1 and not p.startswith("/usr/local")]
