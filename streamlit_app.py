6d528a3c2fde099512de03312b9d1dfe5a7957c0
Wen Jiang <1855485+wjiang@users.noreply.github.com>
2022-07-26 11:00:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,14 @@ import shutil
 from pathlib import Path
 import streamlit as st
 
-st.info(sys.path)
-st.info(sys.executable)
-st.info(dir(sys))
-st.info(shutil.which("python"))
-st.info(shutil.which("conda"))
-st.info(shutil.which("streamlit"))
-
-for p in sys.path:
-  p = Path(p)
-  if p.is_dir():
-    st.info(f"{p}: {[str(pp) for pp in Path(p).glob('*')]}")
-  else:
-    st.info(f"{p}")
-    
-for p in "/usr/local /usr/local/bin /usr/local/share /usr/local/share/cctbx /home/appuser/venv /home/appuser/venv/bin /home/appuser/venv/share /home/appuser/venv/share/cctbx /home/appuser/.conda /home/appuser/.conda/bin /home/appuser/.conda/share /home/appuser/.conda/share/cctbx".split():
-  p = Path(p)
-  if not p.exists():
-    st.info(f"{p}: does not exist")
-    continue
-  if p.is_dir():
-    st.info(f"{p}: {[str(pp) for pp in Path(p).glob('*')]}")
-  else:
-    st.info(f"{p}")
-  
-
-#sys.path = [p for p in sys.path if p.find("3.7")==-1 and not p.startswith("/usr/local")]
-#sys.path.append("/home/appuser/venv/lib/python3.9")
-#st.info(sys.path)
-
+# essential to avoid cctbx import errors
 target = Path("/home/appuser/venv/share/cctbx")
 if not target.exists():
   target.symlink_to("/home/appuser/.conda/share/cctbx")
 
 import cctbx
 import iotbx
+
+st.info(dir(iotbx))
+st.info(dir(cctbx))
+st.info(f"cctbx version: {cctbx.get_version()}")
