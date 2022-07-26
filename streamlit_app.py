d10cad4ff97fd9efdc487ff52ffb61baa7013a8e
Wen Jiang <1855485+wjiang@users.noreply.github.com>
2022-07-25 22:15:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import sys
 import streamlit as st
 
 st.info(sys.path)
-st.info(__file__)
+
+sys.path = [p for p in sys.path if p.find("3.7")==-1 and not p.startswith("/usr/local")]
+st.info(sys.path)
 
 import cctbx
 import iotbx
