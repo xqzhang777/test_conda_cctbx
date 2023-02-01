368873b70258e7a7ac464ea020a8c664ce605ec6
xqzhang777 <105519489+xqzhang777@users.noreply.github.com>
2023-02-01 16:57:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,24 +3,12 @@ import shutil
 from pathlib import Path
 import streamlit as st
 
-# essential to avoid cctbx import errors
-target = Path("/home/appuser/venv/share/cctbx")
-if not target.exists():
-  target.symlink_to("/home/appuser/.conda/share/cctbx")
-
-import cctbx
-import iotbx
-
-st.info(dir(iotbx))
-st.info(dir(cctbx))
-st.info(f"cctbx version: {cctbx.get_version()}")
-
 sys.path += ["/home/appuser/venv/lib/python3.9/lib-dynload"]
-from cctbx.array_family import flex
-st.info(flex.__file__)
 
 st.info(list(Path("/").rglob("*hmmer*")))
 st.info(list(Path("/").rglob("*findmysequence*")))
+
+st.info(list(Path("/home/appuser/venv/")))
 
 st.info(os.environ["PATH"])
 st.info(shutil.which("phmmer"))
